99eba2370d128fb6a6fa01d55655d2a1050dd97b
guzmanico23 <guz.olivera@gmail.com>
2017-10-26 19:29:27 -0300

    Agregue un comentario

--- rentacoder_core/settings/production.py
+++ rentacoder_core/settings/production.py
@@ -3,6 +3,8 @@ from .base import *
 DOMAIN = "34.212.201.181"
 ALLOWED_HOSTS = [DOMAIN]
 
+#Hola soy un comentario
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
