1f1d02eddbebe3708ac77e5b0fba908b9e88fae9
Noya <martinnoya@gmail.com>
2017-10-30 21:06:58 -0300

    Added technologies to project context

--- rentacoder_app/views.py
+++ rentacoder_app/views.py
@@ -92,6 +92,7 @@ def project(request, pk):
     context = {
         "project": project,
         "job_offers": job_offers,
+        "technologies": list(project.technologies.all().values_list("name", flat=True)),
         "own_project": request.user == project.user,
         "questions": project.projectquestion_set.all(),  # TODO: Private questions, private answers
         "question_form": ProjectQuestionForm(),
